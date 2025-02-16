8e6878b9b36c77367dae0b4f5c86be640eae1502
LogapriyaS12 <logapriya9363@gmail.com>
2025-02-15 22:49:08 -0500

    script.js

--- Frontend/Task_1/script.js
+++ Frontend/Task_1/script.js
@@ -1,1 +1,17 @@
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  
+  const mobileMenu = document.getElementById('mobile-menu');
+  const navLinks = document.querySelector('.nav-links');
+  
+  mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
